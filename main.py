eb99cb3803c01d567261eb141dfe4ffa6f229ec4
Aryan Bhat <aryanbhat.2017@gmail.com>
2026-04-26 22:18:33 -0400

    removed data folder from tracking

---
+++ main.py
@@ -0,0 +1,8 @@
+import torch
+from torch.nn import nn
+import numpy as np
+import matplotlib.pyplot as plt
+from torchvision import datasets, transforms, models
+from torch.utils.data import DataLoader, Subset
+
+    
