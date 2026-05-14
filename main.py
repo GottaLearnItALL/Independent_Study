587caa9c643083e46c6bbf48cda483f74db630f3
Aryan Bhat <aryanbhat.2017@gmail.com>
2026-05-13 23:27:17 -0400

    Clean repo for submission - remove pycache, redundant notebooks, and root-level images

--- main.py
+++
@@ -1,8 +0,0 @@
-import torch
-from torch.nn import nn
-import numpy as np
-import matplotlib.pyplot as plt
-from torchvision import datasets, transforms, models
-from torch.utils.data import DataLoader, Subset
-
-    
